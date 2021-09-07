7b66c5ea08d05c5c132e135280ed6e2ba963f194
José Ibañez <appibanez@gmail.com>
2021-09-07 22:59:14 +0200

    separate routes and controller

---
+++ routes/usuarios.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+
+const {
+  usuariosGet,
+  usuariosDelete,
+  usuariosPost,
+  usuariosPut,
+  usuariosPatch,
+} = require("../controllers/usuarios");
+
+const router = Router();
+
+router.get("/", usuariosGet);
+
+router.put("/", usuariosPut);
+
+router.post("/", usuariosPost);
+
+router.delete("/", usuariosDelete);
+
+router.patch("/", usuariosPatch);
+
+module.exports = router;
