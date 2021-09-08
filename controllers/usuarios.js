2c6dab808329e0e3de50b49a2c02d44e4cf79db1
José Ibañez <appibanez@gmail.com>
2021-09-08 10:45:14 +0200

    segment parameters and query

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,10 @@
-const { response } = require("express");
+const { response, request } = require("express");
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+  const query = req.query;
   res.json({
     msg: "get API - controlador",
+    query,
   });
 };
 
@@ -13,8 +15,11 @@ const usuariosPost = (req, res = response) => {
 };
 
 const usuariosPut = (req, res = response) => {
+  const id = req.params.id;
+
   res.json({
     msg: "put API - controlador",
+    id,
   });
 };
 
